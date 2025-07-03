67153fee37159d7283137879662a38458a56419f
diyaprajapati <diyabprajapati2005@gmail.com>
2025-07-03 18:07:42 +0530

    feat: return sum of two comma separated numbers

--- src/main/java/com/diya/stringcalculator/StringCalculator.java
+++ src/main/java/com/diya/stringcalculator/StringCalculator.java
@@ -2,7 +2,16 @@ package com.diya.stringcalculator;
 
 public class StringCalculator {
     public int add(String numbers) {
+        //for empty string
         if(numbers.isEmpty()) return 0;
+
+        //for two numbers separated by comma
+        if(numbers.contains(",")) {
+            String[] numbersArray = numbers.split(",");
+            return Integer.parseInt(numbersArray[0]) + Integer.parseInt(numbersArray[1]);
+        }
+
+        //for single numbers
         return Integer.parseInt(numbers);
     }
 }
