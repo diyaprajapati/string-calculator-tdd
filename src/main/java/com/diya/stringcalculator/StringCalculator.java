ee0b7884ddb2a08ea872d7b377b3297bff6536bb
diyaprajapati <diyabprajapati2005@gmail.com>
2025-07-03 18:01:57 +0530

    feat: return number itself when one number is passed

--- src/main/java/com/diya/stringcalculator/StringCalculator.java
+++ src/main/java/com/diya/stringcalculator/StringCalculator.java
@@ -3,6 +3,6 @@ package com.diya.stringcalculator;
 public class StringCalculator {
     public int add(String numbers) {
         if(numbers.isEmpty()) return 0;
-        return 0; //default
+        return Integer.parseInt(numbers);
     }
 }
